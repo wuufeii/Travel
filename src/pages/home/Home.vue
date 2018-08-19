6321b491f6d5cabb983617841bfa4791c640f8d6
wufei <wuufeii@qq.com>
2018-08-19 10:06:04 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,14 @@
 <template>
-	<div>
-		<div>home</div>
-	</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default{
-  name: 'Home'
+  name: 'Home',
+  components:{HomeHeader}
 }
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  display:flex
+  line-height:.86rem
+  background:$bgColor
+  color:#fff
+  .header-left
+    width:.64rem
+    float:left
+    .back-icon
+      display:block
+      font-size:.4rem
+      text-align:center
+  .header-input
+    flex:1
+    height:.64rem
+    line-height.64rem
+    margin-top:.11rem
+    margin-left:.2rem
+    padding-left:.2rem
+    background-color:#fff
+    border-radius:.1rem
+    color:#ccc
+  .header-right
+    width:1.24rem
+    float:right
+    text-align:center
+    .arrow-icon
+      font-size:.24rem
+      margin-left:-.04rem
+</style>
